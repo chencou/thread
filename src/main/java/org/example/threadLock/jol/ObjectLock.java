d7c15b35260aa498e1045866815a68dc2941a285
cheng <chengfuse@outlook.com>
2023-06-18 17:17:58 +0800

    生产者消费者

---
+++ src/main/java/org/example/threadLock/jol/ObjectLock.java
@@ -0,0 +1,29 @@
+package org.example.threadLock.jol;
+
+import com.itlgl.java.util.ByteUtils;
+
+/**
+ * 对象布局分析
+ */
+
+public class ObjectLock {
+
+    private Integer amount =0; //整形字段占用4字节
+
+    public void increase(){
+        synchronized (this){
+            amount++;
+        }
+    }
+
+    /**
+     * 输出十六进制，小端模式的hashCode
+     */
+    public String hexHash(){
+        //对象的原始hashCode，Java默认为大小端模式
+        int  hashCode =this.hashCode();
+
+        //转成小端模式的字节数组
+        return  null;
+    }
+}
